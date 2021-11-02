13a9bb4f290650fc67c52091d3bc7ad586407d6b
jeremymenke <87037301+jeremymenke@users.noreply.github.com>
2021-11-02 16:04:01 -0400

    First big commit.

--- examples/cp4d_4.0/variables.tf
+++ examples/cp4d_4.0/variables.tf
@@ -1,5 +1,6 @@
 variable "operator_namespace" {
   description = "Needs to be ibm-common-services."
+  default = "ibm-common-services"
 }
 
 variable "cluster_id" {
@@ -12,15 +13,12 @@ variable "ibmcloud_api_key" {
 
 variable "region" {
   description = "Region of the cluster"
+  default = "us-south"
 }
 
 variable "resource_group_name" {
   description = "Resource group that the cluster is created in"
-}
-
-variable "enable" {
-  default     = true
-  description = "If set to true installs Cloud-Pak for Data on the given cluster"
+  default = "cloud-pak-sandbox-ibm"
 }
 
 variable "on_vpc" {
@@ -29,16 +27,20 @@ variable "on_vpc" {
   description = "If set to true, lets the module know cluster is using VPC Gen2"
 }
 
-variable "cluster_config_path" {
-  description = "Path to the Kubernetes configuration file to access your cluster"
+variable "accept_cpd_license" {
+  type        = bool
+  description = "Do you accept the cpd license agreements? This includes any modules chosen as well. `true` or `false`"
 }
 
-variable "openshift_version" {
-  default     = "4.6"
-  description = "Openshift version installed in the cluster"
+// Prereq
+variable "worker_node_flavor" {
+  type        = string
+  description = "Flavor used to determine worker node hardware"
+  default     = "b3c.16x64"
 }
 
 variable "entitled_registry_key" {
+  type        = string
   description = "Get the entitlement key from https://myibm.ibm.com/products-services/containerlibrary"
 }
 
@@ -48,131 +50,98 @@ variable "entitled_registry_user_email" {
   default     = "cp"
 }
 
-variable "accept_cpd_license" {
-  type        = bool
-  description = "Do you accept the cpd license agreements? This includes any modules chosen as well. `true` or `false`"
-}
-
-variable "cpd_project_name" {
-  type        = string
-  default     = "cp4d"
-  description = "Name of the project namespace"
-}
-// Prereq
-variable "worker_node_flavor" {
-  type        = string
-  description = "Flavor used to determine worker node hardware"
-}
-
-variable "portworx_is_ready" {
-  type = any
-  default = null
-}
 // Modules available to install on CP4D
-
-variable "empty_module_list" {
-  default     = true
-  type        = bool
-  description = "Determine if any modules need to be installed for CP4D"
-}
 
 variable "install_wsl" {
   default = false
-  type = string
+  type = bool
   description = "Install WSL module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_aiopenscale" {
   default = false
-  type = string
+  type = bool
   description = "Install AI Open Scale module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_wml" {
   default = false
-  type = string
+  type = bool
   description = "Install Watson Machine Learning module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_wkc" {
   default = false
-  type = string
+  type = bool
   description = "Install Watson Knowledge Catalog module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_dv" {
   default = false
-  type = string
+  type = bool
   description = "Install Data Virtualization module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_spss" {
   default = false
-  type = string
+  type = bool
   description = "Install SPSS module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_cde" {
   default = false
-  type = string
+  type = bool
   description = "Install CDE module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_spark" {
   default = false
-  type = string
+  type = bool
   description = "Install Analytics Engine powered by Apache Spark module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_dods" {
   default = false
-  type = string
+  type = bool
   description = "Install DODS module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_ca" {
   default = false
-  type = string
+  type = bool
   description = "Install CA module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_ds" {
   default = false
-  type = string
+  type = bool
   description = "Install DS module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_db2oltp" {
   default = false
-  type = string
+  type = bool
   description = "Install DB2OLTP module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_db2wh" {
   default = false
-  type = string
+  type = bool
   description = "Install DB2WH module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_big_sql" {
   default = false
-  type = string
+  type = bool
   description = "Install Big SQL module. Only for Cloud Pak for Data v4.0"
 }
 
 variable "install_wsruntime" {
   default = false
-  type = string
-  description = "Install WS Runtime. Only for Cloud Pak for Data v4.0"
+  type = bool
+  description = "Install WS Runtime."
 }
 
 locals {
-  namespace                = "default"
-  entitled_registry        = "cp.icr.io"
-  entitled_registry_user   = "cp"
-  docker_registry          = "cp.icr.io" // Staging: "cp.stg.icr.io/cp/cpd"
-  docker_username          = "cp"               // "ekey"
-  entitled_registry_key    = chomp(var.entitled_registry_key)
-  openshift_version_regex  = regex("(\\d+).(\\d+)(.\\d+)*(_openshift)*", var.openshift_version)
-  openshift_version_number = local.openshift_version_regex[3] == "_openshift" ? tonumber("${local.openshift_version_regex[0]}.${local.openshift_version_regex[1]}") : 0
+  cluster_config_path = "./.kube/config"
 }
